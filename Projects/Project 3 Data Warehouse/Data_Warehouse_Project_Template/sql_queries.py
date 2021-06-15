aebce4beb6b4269202572967f182303779201667
9ooDa <dayeonkukr@gmail.com>
2021-06-16 01:43:43 +0900

    errors fixed

--- Projects/Project 3 Data Warehouse/Data_Warehouse_Project_Template/sql_queries.py
+++ Projects/Project 3 Data Warehouse/Data_Warehouse_Project_Template/sql_queries.py
@@ -123,14 +123,14 @@ staging_events_copy = ("""
     COPY staging_events FROM {}
         credentials 'aws_iam_role={}'
         format as json {}
-        gzip region 'us-west-2';
+        region 'us-west-2';
 """).format(LOG_DATA, ARN, LOG_JSONPATH)
 
 staging_songs_copy = ("""
     COPY staging_songs FROM {}
         credentials 'aws_iam_role={}'
         format as json 'auto'
-        gzip region 'us-west-2';
+        region 'us-west-2';
 """).format(SONG_DATA, ARN)
 
 # FINAL TABLES
@@ -171,7 +171,7 @@ time_table_insert = ("""
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
+create_table_queries = [staging_events_table_create, staging_songs_table_create, user_table_create, artist_table_create, song_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
